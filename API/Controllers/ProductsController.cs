3a6d32862d0c65ab840e84a1360f0f316b7e6f4c
Goh Sheen An <sheen.an.goh@gmail.com>
2020-06-01 02:33:04 +0800

    Added Data Seeding, Migration, Include queries

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,30 +1,39 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
-using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers {
 
   [ApiController]
   [Route("api/[controller]")]
   public class ProductsController : ControllerBase {
-    private readonly StoreContext _context;
-    public ProductsController(StoreContext context) {
-      _context = context;
+    private readonly IProductRepository _repo;
+    public ProductsController(IProductRepository productRepository) {
+      _repo = productRepository;
     }
 
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetProducts() {
-      var products = await _context.Products.ToListAsync();
+      var products = await _repo.GetProductsAsync();
       return Ok(products);
 
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id) {
-      return await _context.Products.FindAsync(id);
+      return await _repo.GetProductByIdAsync(id);
+    }
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProductBrands() {
+      return Ok(await _repo.GetProductBrandsAsync());
+    }
+
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes() {
+      return Ok(await _repo.GetProductTypesAsync());
     }
   }
 }
