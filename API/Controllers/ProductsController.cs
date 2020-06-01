7d0d7cbbea807f3bd9d94bf7edba0ef438552c1a
Goh Sheen An <sheen.an.goh@gmail.com>
2020-06-01 18:40:21 +0800

    Added dtos, automapper, specifications and generic

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,39 +1,64 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers {
+namespace API.Controllers
+{
 
   [ApiController]
   [Route("api/[controller]")]
-  public class ProductsController : ControllerBase {
-    private readonly IProductRepository _repo;
-    public ProductsController(IProductRepository productRepository) {
-      _repo = productRepository;
+  public class ProductsController : ControllerBase
+  {
+    private readonly IGenericRepository<Product> _productsRepo;
+    private readonly IGenericRepository<ProductBrand> _productBrandsRepo;
+    private readonly IGenericRepository<ProductType> _productTypesRepo;
+    private readonly IMapper _mapper;
+
+    public ProductsController(IGenericRepository<Product> productsRepo,
+      IGenericRepository<ProductBrand> productBrandsRepo,
+      IGenericRepository<ProductType> productTypesRepo,
+      IMapper mapper)
+    {
+      _productsRepo = productsRepo;
+      _productBrandsRepo = productBrandsRepo;
+      _productTypesRepo = productTypesRepo;
+      _mapper = mapper;
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts() {
-      var products = await _repo.GetProductsAsync();
-      return Ok(products);
+    public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+    {
+      var spec = new ProductsWithTypesAndBrandsSpecification();
+      var products = await _productsRepo.ListAsync(spec);
+      return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
 
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Product>> GetProduct(int id) {
-      return await _repo.GetProductByIdAsync(id);
+    public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
+    {
+      var spec = new ProductsWithTypesAndBrandsSpecification(id);
+      var product = await _productsRepo.GetEntityWithSpec(spec);
+
+      return _mapper.Map<Product, ProductToReturnDto>(product);
     }
 
     [HttpGet("brands")]
-    public async Task<ActionResult<IReadOnlyList<Product>>> GetProductBrands() {
-      return Ok(await _repo.GetProductBrandsAsync());
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProductBrands()
+    {
+      return Ok(await _productBrandsRepo.ListAllAsync());
     }
 
     [HttpGet("types")]
-    public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes() {
-      return Ok(await _repo.GetProductTypesAsync());
+    public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+    {
+      return Ok(await _productTypesRepo.ListAllAsync());
     }
   }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using API.Helpers;
+using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -7,35 +9,45 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace API {
-  public class Startup {
+namespace API
+{
+  public class Startup
+  {
     private readonly IConfiguration _config;
 
-    public Startup(IConfiguration config) {
+    public Startup(IConfiguration config)
+    {
       _config = config;
     }
 
     // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services) {
+    public void ConfigureServices(IServiceCollection services)
+    {
       services.AddControllers();
+      services.AddAutoMapper(typeof(MappingProfiles));
       services.AddScoped<IProductRepository, ProductRepository>();
+      services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
       services.AddDbContext<StoreContext>(x =>
         x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-      if (env.IsDevelopment()) {
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
         app.UseDeveloperExceptionPage();
       }
 
       app.UseHttpsRedirection();
 
       app.UseRouting();
+      app.UseStaticFiles();
 
       app.UseAuthorization();
 
-      app.UseEndpoints(endpoints => {
+      app.UseEndpoints(endpoints =>
+      {
         endpoints.MapControllers();
       });
     }
